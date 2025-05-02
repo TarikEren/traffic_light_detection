cb39e3ce3c1e2a1921306a7cae36bf02a44e402d
TarikEren <1210606015@nku.edu.tr>
2025-05-03 00:16:33 +0300

    Overhaul

--- detect.py
+++ detect.py
@@ -6,91 +6,112 @@ from pathlib import Path
 
 from ultralytics import YOLO
 
-from utils import get_latest_train, get_train_directories, get_latest_detect_suffix
+from utils import get_latest_train, get_train_directories, get_latest_detect_suffix, video_inference, image_inference
 
-# Setting up parser
-parser = argparse.ArgumentParser(prog="Traffic Light Detector",
-                                 description="Detects traffic lights in a given image or video")
+VIDEO_EXT = [".mp4", ".avi", ".mp3", ".mpeg", ".mpg"]
+IMG_EXT = [".bmp",".jpeg",".jpg",".png",".tif",".tiff"]
 
-# Setting up arguments for the parser
-# Target directory or file
-parser.add_argument("-t", "--target",
-                    help="Target directory or file name")
+def parse_args():
+    # Setting up parser
+    parser = argparse.ArgumentParser(prog="Traffic Light Detector",
+                                    description="Detects traffic lights in a given image or video")
 
-# Model to use
-parser.add_argument("-m", "--model",
-                    help="Path to the model to use",
-                    default="traffic_light_model.pt")
+    # Setting up arguments for the parser
+    # Target directory or file
+    parser.add_argument("-t", "--target",
+                        help="Target directory or file name")
 
-# Parse the given arguments
-args = parser.parse_args()
+    # Model to use
+    parser.add_argument("-m", "--model",
+                        help="Path to the model to use",
+                        default="traffic_light_model.pt")
 
-if args.target is None:
-    raise ValueError("'target' argument should not be omitted")
+    # Parse the given arguments
+    args = parser.parse_args()
+    return args
 
-if not os.path.exists(args.target):
-    raise FileNotFoundError(f"Failed to find directory {args.target}")
 
-if args.model == "latest":
-    if not os.path.exists("runs"):
-        print("WARN: 'runs' directory not found, using default model")
-        if not os.path.exists("traffic_light_model.pt"):
-            raise FileNotFoundError("Failed to find the model 'traffic_light_model'. Please reinstall the program")
-        MODEL = "traffic_light_model.pt"
+def main(args):
+    if args.target is None:
+        raise ValueError("'target' argument should not be omitted")
+
+    if not os.path.exists(args.target):
+        raise FileNotFoundError(f"Failed to find directory {args.target}")
+
+    if args.model == "latest":
+        if not os.path.exists("runs"):
+            print("WARN: 'runs' directory not found, using default model")
+            if not os.path.exists("traffic_light_model.pt"):
+                raise FileNotFoundError("Failed to find the model 'traffic_light_model'. Please reinstall the program")
+            MODEL = "traffic_light_model.pt"
+        else:
+            train_dirs = get_train_directories()
+            MODEL = os.path.join("runs", "detect", get_latest_train(train_directories=train_dirs), "weights", "last.pt")
     else:
-        train_dirs = get_train_directories()
-        MODEL = os.path.join("runs", "detect", get_latest_train(train_directories=train_dirs), "weights", "last.pt")
-else:
-    MODEL = args.model
+        MODEL = args.model
 
-if os.path.isdir(args.target):
-    # Get all target files from the directory
-    target = []
-    for file in os.listdir(args.target):
-        target.append(os.path.join("./", args.target, file))
-else:
-    target = args.target
+    if os.path.isdir(args.target):
+        # Get all target files from the directory
+        target = []
+        for file in os.listdir(args.target):
+            target.append(os.path.join("./", args.target, file))
+    else:
+        target = args.target
 
-print(f"""########## DETECTION INFO ##########
+    print(f"""{"#"*30} DETECTION INFO {"#"*30}
     target directory: {args.target}
     model directory: {MODEL}
-########## END DETECTION INFO ##########""")
+{"#"*30} END DETECTION INFO {"#"*30}""")
 
-# Get the model
-model = YOLO(MODEL)
+    # Get the model
+    model = YOLO(MODEL)
 
-# Get the results
-results = model(target, stream=True)
+    # If detections directory doesn't exist
+    if not os.path.exists("detections"):
+        # Create one
+        os.mkdir("detections")
 
-# If detections directory doesn't exist
-if not os.path.exists("detections"):
-    # Create one
-    os.mkdir("detections")
+    # Get the latest detect suffix
+    latest_detect_suffix = get_latest_detect_suffix(os.listdir("detections"))
 
-# Get the latest detect suffix
-latest_detect_suffix = get_latest_detect_suffix(os.listdir("detections"))
+    # Create the target directory name from the suffix
+    new_detect_name = "detect" + str(latest_detect_suffix + 1)
 
-# Create the target directory name from the suffix
-new_detect_name = "detect" + str(latest_detect_suffix + 1)
+    # Check if the file exists just in case
+    if not os.path.exists(os.path.join("detections", new_detect_name)):
+        # If not, make it
+        os.mkdir(os.path.join("detections", new_detect_name))
 
-# Check if the file exists just in case
-if not os.path.exists(os.path.join("detections", new_detect_name)):
-    # If not, make it
-    os.mkdir(os.path.join("detections", new_detect_name))
+    if os.path.isdir(target):
+        for elem in target:
+            file_name, file_ext = os.path.splitext(elem)
+            if file_ext in VIDEO_EXT:
+                video_inference(model=model,
+                                target_file=elem,
+                                latest_predict=new_detect_name,
+                                file_name=f"{Path(file_name).stem}{file_ext}")
+            elif file_ext in IMG_EXT:
+                image_inference(model=model,
+                                target_file=elem,
+                                latest_predict=new_detect_name)
+            else:
+                print(f"WARN: Invalid file extension {file_ext} in file {file_name}; skipping")
+    else:
+        file_name, file_ext = os.path.splitext(target)
+        if file_ext in VIDEO_EXT:
+            video_inference(model=model,
+                target_file=target,
+                latest_predict=new_detect_name,
+                file_name=f"{Path(file_name).stem}{file_ext}")
+        elif file_ext in IMG_EXT:
+            image_inference(model=model,
+                            target_file=target,
+                            latest_predict=new_detect_name)
+        else:
+            print(f"WARN: Invalid file extension {file_ext} in file {file_name}; skipping")
 
-# Process results list
-for result in results:
-    # Get the target name
-    target_name = str(Path(result.path).stem) + str(Path(result.path).suffix)
-    # Get the target path
-    target_path = os.path.join("detections", new_detect_name, target_name)
-    print(f"Target path: {target_path}")
+    print(f"INFO: Results are saved to 'detections/{new_detect_name}'")
 
-    boxes = result.boxes                    # Boxes object for bounding box outputs
-    masks = result.masks                    # Masks object for segmentation masks outputs
-    keypoints = result.keypoints            # Keypoints object for pose outputs
-    probs = result.probs                    # Probs object for classification outputs
-    obb = result.obb                        # Oriented boxes object for OBB outputs
-    result.save(filename=target_path)       # Save to disk
-
-print(f"INFO: Results are saved to 'detections/{new_detect_name}'")
+if __name__ == "__main__":
+    args = parse_args()
+    main(args=args)
